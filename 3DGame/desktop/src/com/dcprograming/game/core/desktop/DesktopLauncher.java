9df29cd4544e21a4d0553a869062832edabeadc1
BlueEvil777 <zecoltonisawesome@gmail.com>
2018-05-02 14:55:06 -0600

    Initial LibGDX Project

---
+++ 3DGame/desktop/src/com/dcprograming/game/core/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dcprograming.game.core.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dcprograming.game.core.Core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Core(), config);
+	}
+}
