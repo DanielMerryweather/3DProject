61f57cb92c04ad713ea7abfc0e0583f0f8789719
BlueEvil777 <zecoltonisawesome@gmail.com>
2018-05-03 15:02:09 -0600

    Now has two balls and they move.

--- 3DGame/core/src/com/dcprograming/game/core/Core.java
+++ 3DGame/core/src/com/dcprograming/game/core/Core.java
@@ -23,34 +23,46 @@ public class Core extends ApplicationAdapter {
 	ModelBuilder builder;
 	Model box;
 	ModelInstance realBox;
+	Vector3 boxMotion;
+	ModelInstance realBox2;
+	Vector3 boxMotion2;
 	Environment world;
 
 	@Override
 	public void create() {
 
 		cam = new PerspectiveCamera(70, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.translate(0, 1, -5);
+		cam.translate(0, 1, -15);
 		cam.lookAt(new Vector3(0, 0, 0));
 		cam.update();
 		Gdx.gl.glEnable(GL20.GL_BLEND);
 		builder = new ModelBuilder();
 		box = builder.createSphere(2f, 2f, 2f, 16, 16, new Material(ColorAttribute.createDiffuse(Color.BLUE), ColorAttribute.createSpecular(1, 1f, 0.9f, 0.1f)), Usage.Normal | Usage.Position);
-		realBox = new ModelInstance(box, 0, 0, 0);
+		realBox = new ModelInstance(box, -6, 0, 0);
+		realBox2 = new ModelInstance(box, 6, 0, 0);
+		boxMotion = new Vector3(0.5f, 0, 0);
+		boxMotion2 = new Vector3(0, 5, 0);
 		mb = new ModelBatch();
 		world = new Environment();
-		world.set(new ColorAttribute(ColorAttribute.AmbientLight, 1, 0.1f, 0.1f, 1));
+		world.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1));
 		world.set(new ColorAttribute(ColorAttribute.Fog, 1f, 1f, 1f, 1));
 		world.add(new DirectionalLight().set(Color.WHITE, new Vector3(1, -1, 1)));
 	}
 
 	@Override
 	public void render() {
+
+		float dtime = Gdx.graphics.getDeltaTime();
+		boxMotion2.y -= 9.81f / 4;
+		realBox.transform.translate(boxMotion.x * dtime, boxMotion.y * dtime, boxMotion.z * dtime);
+		realBox2.transform.translate(boxMotion2.x * dtime, boxMotion2.y * dtime, boxMotion2.z * dtime);
+
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-		cam.rotateAround(Vector3.Zero, new Vector3(0, 1, 0), 0.1f);
-		cam.update();
+
 		mb.begin(cam);
 		mb.render(realBox, world);
+		mb.render(realBox2, world);
 		mb.end();
 	}
 
