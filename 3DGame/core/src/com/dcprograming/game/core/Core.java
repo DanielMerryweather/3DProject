9b0a6d3440389b2419899f9bba1cc9c430a93ef3
Peary <36944647+PearyD@users.noreply.github.com>
2018-05-10 16:01:50 -0600

    Made things cooler

--- 3DGame/core/src/com/dcprograming/game/core/Core.java
+++ 3DGame/core/src/com/dcprograming/game/core/Core.java
@@ -12,58 +12,86 @@ import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalShadowLight;
+import com.badlogic.gdx.graphics.g3d.utils.DepthShaderProvider;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
 
 public class Core extends ApplicationAdapter {
 
 	PerspectiveCamera cam;
-	ModelBatch mb;
-	ModelBuilder builder;
-	Model box;
-	ModelInstance realBox;
-	Vector3 boxMotion;
-	ModelInstance realBox2;
-	Vector3 boxMotion2;
+	ModelBatch spheres;
+	Model sphere;
+	Model plane;
+	ModelInstance sph;
+	ModelInstance pln;
 	Environment world;
-
+	
+	float y = 0f;
+	
+	ModelBuilder mb = new ModelBuilder();
+	
+	Vector3 camRotation = new Vector3(0,0,0);
+	
+	DirectionalShadowLight sl;
+	ModelBatch sb;
+	
+	@SuppressWarnings("deprecation")
 	@Override
 	public void create() {
 
 		cam = new PerspectiveCamera(70, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.translate(0, 1, -15);
+		cam.translate(0, 1, -3);
 		cam.lookAt(new Vector3(0, 0, 0));
 		cam.update();
 		Gdx.gl.glEnable(GL20.GL_BLEND);
-		builder = new ModelBuilder();
-		box = builder.createSphere(2f, 2f, 2f, 16, 16, new Material(ColorAttribute.createDiffuse(Color.BLUE), ColorAttribute.createSpecular(1, 1f, 0.9f, 0.1f)), Usage.Normal | Usage.Position);
-		realBox = new ModelInstance(box, -6, 0, 0);
-		realBox2 = new ModelInstance(box, 6, 0, 0);
-		boxMotion = new Vector3(0.5f, 0, 0);
-		boxMotion2 = new Vector3(0, 5, 0);
-		mb = new ModelBatch();
+		sphere = mb.createSphere(1f, 1f, 1f, 16, 16, new Material(ColorAttribute.createDiffuse(Color.GREEN)), Usage.Normal | Usage.Position);
+		plane = mb.createBox(10f, 0.1f, 10f, new Material(ColorAttribute.createDiffuse(Color.GRAY)), Usage.Normal | Usage.Position);
+		sph = new ModelInstance(sphere, 0, 2f, 0);
+		pln = new ModelInstance(plane);
+		spheres = new ModelBatch();
 		world = new Environment();
 		world.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1));
-		world.set(new ColorAttribute(ColorAttribute.Fog, 1f, 1f, 1f, 1));
-		world.add(new DirectionalLight().set(Color.WHITE, new Vector3(1, -1, 1)));
+		world.add((sl = new DirectionalShadowLight(1024, 1024, 15f, 15f, .1f, 50f)).set(1f, 1f, 1f, new Vector3(-1f,-1f,-1f)));
+		world.shadowMap = sl;
+		
+		sb = new ModelBatch(new DepthShaderProvider());
+		//world.add(new DirectionalLight().set(Color.WHITE, new Vector3(1, -1, 1)));
 	}
 
 	@Override
 	public void render() {
 
 		float dtime = Gdx.graphics.getDeltaTime();
-		boxMotion2.y -= 9.81f / 4;
-		realBox.transform.translate(boxMotion.x * dtime, boxMotion.y * dtime, boxMotion.z * dtime);
-		realBox2.transform.translate(boxMotion2.x * dtime, boxMotion2.y * dtime, boxMotion2.z * dtime);
-
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		
+		sl.begin(Vector3.Zero, cam.direction);
+		sb.begin(sl.getCamera());
+		sb.render(sph);
+		
+		sb.end();
+		sl.end();
+		
+		Gdx.gl.glClearColor(0.2f, 0.7f, 1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-
-		mb.begin(cam);
-		mb.render(realBox, world);
-		mb.render(realBox2, world);
-		mb.end();
+		
+		sph.transform.translate(0, y, 0);
+		if(sph.transform.getTranslation(new Vector3()).y <= 0.5f){
+			y = -y*1f;
+			sph.transform.setTranslation(new Vector3(0, 0.5f, 0));
+		}else{
+			y -= 9.81*(dtime/20);
+		}
+		//System.out.println(sph.transform.getTranslation(new Vector3()).y);
+		
+		cam.rotate(Vector3.Y, -0.3f * Gdx.input.getDeltaY());
+		cam.rotate(Vector3.X, 0.3f * Gdx.input.getDeltaY());
+		cam.update();
+		
+		spheres.begin(cam);
+		spheres.render(sph, world);
+		spheres.render(pln, world);
+		spheres.end();
 	}
 
 	@Override
