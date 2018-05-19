39c60d2ff922631ebff4ab641f40b555d28b79a5
Peary <36944647+PearyD@users.noreply.github.com>
2018-05-19 16:32:26 -0600

    Adjustments
    Implemented Player class, adjusted render models, and fixed shadow rendering position.

--- 3DGame/core/src/com/dcprograming/game/core/Core.java
+++ 3DGame/core/src/com/dcprograming/game/core/Core.java
@@ -1,11 +1,12 @@
 package com.dcprograming.game.core;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
@@ -16,25 +17,22 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalShadowLight;
 import com.badlogic.gdx.graphics.g3d.utils.DepthShaderProvider;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
-import com.badlogic.gdx.math.Quaternion;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
 public class Core extends ApplicationAdapter {
 
-	PerspectiveCamera cam;
-	ModelBatch spheres;
+	//PerspectiveCamera cam;
+	ModelBatch models;
 	Model sphere;
 	Model plane;
-	ModelInstance sph;
-	ModelInstance pln;
-	Environment world;
-	
 	Model coords;
-	ModelInstance crds;
+	ArrayList<ModelInstance> instances = new ArrayList<ModelInstance>();
+	Environment world;
 	
 	float grav = -9.81f;
 	float y = 0f;
+	
+	Player p;
 	
 	ModelBuilder mb = new ModelBuilder();
 	
@@ -44,29 +42,27 @@ public class Core extends ApplicationAdapter {
 	@SuppressWarnings("deprecation")
 	@Override
 	public void create() {
-
-		cam = new PerspectiveCamera(110, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.near = 0.1f;
-		cam.far = 50f;
-		cam.translate(0, 1, -3);
-		cam.lookAt(new Vector3(0, 0, 0));
-		cam.update();
+		p = new Player(0, 1, -3, 110, 0, 0, 0);
 		Gdx.gl.glEnable(GL20.GL_BLEND);
 		sphere = mb.createSphere(1f, 1f, 1f, 16, 16, new Material(ColorAttribute.createDiffuse(Color.GREEN)), Usage.Normal | Usage.Position);
-		plane = mb.createBox(3f, 0.1f, 3f, new Material(ColorAttribute.createDiffuse(Color.WHITE)), Usage.Normal | Usage.Position);
-		sph = new ModelInstance(sphere, 0, 2f, 0);
-		pln = new ModelInstance(plane);
-		spheres = new ModelBatch();
+		plane = mb.createBox(3f, 0.1f, 3f, new Material(ColorAttribute.createSpecular(Color.WHITE)), Usage.Normal | Usage.Position);
+		instances.add(new ModelInstance(sphere, 0, 2f, 0));
+		//instances.add(new ModelInstance(plane));
+		for(int x=-5;x<5;x++) {
+			for(int y=-5;y<5;y++) {
+				instances.add(new ModelInstance(plane, x*3f, Math.abs(x*x+y*y)*0.1f, y*3f));
+			}
+		}
+		models = new ModelBatch();
 		world = new Environment();
 		world.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1));
 		world.add((sl = new DirectionalShadowLight(1024, 1024, 15f, 15f, .1f, 50f)).set(1f, 1f, 1f, new Vector3(-1f,-1f,-1f)));
 		world.shadowMap = sl;
 		
 		coords = mb.createXYZCoordinates(1f, new Material(ColorAttribute.createDiffuse(Color.RED)), Usage.Normal | Usage.Position);
-		crds = new ModelInstance(coords, 0, 1f, 0);
+		instances.add(new ModelInstance(coords, 0, 1f, 0));
 		
 		sb = new ModelBatch(new DepthShaderProvider());
-		//world.add(new DirectionalLight().set(Color.WHITE, new Vector3(1, -1, 1)));
 	}
 
 	@Override
@@ -82,55 +78,40 @@ public class Core extends ApplicationAdapter {
 		
 		float dtime = Gdx.graphics.getDeltaTime();
 		
-		sl.begin(Vector3.Zero, cam.direction);
-		sb.begin(sl.getCamera());
-		sb.render(sph);
+		sl.begin(p.playerPosition, p.playerCam.direction);
 		
+		sb.begin(sl.getCamera());
+		sb.render(instances);
 		sb.end();
+		
 		sl.end();
 		
 		Gdx.gl.glClearColor(0.2f, 0.7f, 1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 		
-		sph.transform.translate(0, y, 0);
-		if(sph.transform.getTranslation(new Vector3()).y <= 0.5f){
+		instances.get(0).transform.translate(0, y, 0);
+		if(instances.get(0).transform.getTranslation(new Vector3()).y <= 0.5f){
 			y = -y*1f;
-			sph.transform.setTranslation(new Vector3(0, 0.5f, 0));
+			y -= 9.81*(dtime/20);
+			instances.get(0).transform.setTranslation(new Vector3(0, 0.5f, 0));
 		}else{
 			y -= 9.81*(dtime/20);
 		}
 		
 		if(Gdx.input.isCursorCatched()){
-			cam.rotate(Vector3.Y, -0.3f * Gdx.input.getDeltaX());
-			cam.rotate(cam.direction.cpy().crs(Vector3.Y), -0.3f * Gdx.input.getDeltaY());
-			
+			p.rotate(0.3f);
 			float cx =( Gdx.input.isKeyPressed(Keys.A)?1:0) + (Gdx.input.isKeyPressed(Keys.D)?-1:0);
 			float cy =( Gdx.input.isKeyPressed(Keys.SPACE)?1:0) + (Gdx.input.isKeyPressed(Keys.CONTROL_LEFT)?-1:0);
 			float cz =( Gdx.input.isKeyPressed(Keys.W)?1:0) + (Gdx.input.isKeyPressed(Keys.S)?-1:0);
-			Quaternion q = cam.view.getRotation(new Quaternion());
 			float speed = 0.05f;
-			float nx = (float)Math.sin(-q.getYaw()/180*Math.PI)*-speed*cz + (float)Math.sin((90-q.getYaw())/180*Math.PI)*-speed*cx;
-			float ny = (float)Math.cos(-q.getPitch()/180*Math.PI)*speed*cy;
-			float nz = (float)Math.cos(-q.getYaw()/180*Math.PI)*-speed*cz + (float)Math.cos((90-q.getYaw())/180*Math.PI)*-speed*cx;
-			cam.translate(nx, ny, nz);
-			//cam.translate((float)Math.sin(-q.getYaw()/180*Math.PI)*-speed, 0, (float)Math.cos(-q.getYaw()/180*Math.PI)*-speed);//(float)Math.cos(q.getYaw())*speed);
-			//cam.translate((float)Math.sin(-q.getYaw()/180*Math.PI)*-speed, 0, (float)Math.cos(-q.getYaw()/180*Math.PI)*-speed);//(float)Math.cos(q.getYaw())*speed);
-			
-			cam.update();
-			
-			System.out.println(getCamAngle());
+			p.localTranslate(cx*speed, cy*speed, cz*speed);
 		}
 		
-		spheres.begin(cam);
-		spheres.render(sph, world);
-		spheres.render(pln, world);
-		spheres.render(crds, world);
-		spheres.end();
-	}
-	
-	public float getCamAngle()
-	{
-	    return (float)(Math.atan2(cam.up.x, cam.up.y)/Math.PI)*180;
+		p.logInfo();
+		
+		models.begin(p.playerCam);
+		models.render(instances, world);
+		models.end();
 	}
 	
 	@Override
