31d8e1d090951299173f5519101ffef16a965d51
Peary <36944647+PearyD@users.noreply.github.com>
2018-05-29 15:40:42 -0600

    Add files via upload

--- 3DGame/desktop/src/com/dcprograming/game/core/desktop/DesktopLauncher.java
+++ 3DGame/desktop/src/com/dcprograming/game/core/desktop/DesktopLauncher.java
@@ -1,12 +1,24 @@
 package com.dcprograming.game.core.desktop;
 
+import java.awt.Toolkit;
+
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.dcprograming.game.core.Core;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Core(), config);
+		int pixels = (int) Math.min(Toolkit.getDefaultToolkit().getScreenSize().getHeight()/9, Toolkit.getDefaultToolkit().getScreenSize().getWidth()/16);
+		config.width = pixels * 16;
+		config.height = pixels * 9;
+		System.out.println("Initial Screen Resolution: " + config.width + "x" + config.height);
+		config.resizable = false;
+		config.addIcon("Icon.png", FileType.Internal);
+		config.addIcon("Icon32.png", FileType.Internal);
+		config.title = "Quittage";
+		Core game = new Core();
+		new LwjglApplication(game, config);
 	}
 }
