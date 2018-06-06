27f22939e397928b9a0a52633bd8706422578d1f
Peary <36944647+PearyD@users.noreply.github.com>
2018-06-06 17:35:48 -0600

    Add files via upload

--- 3DGame/core/src/com/dcprograming/game/entities/PlayerModel.java
+++ 3DGame/core/src/com/dcprograming/game/entities/PlayerModel.java
@@ -7,21 +7,33 @@ import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
+/**
+ * @author 50018003
+ * By: Daniel Merryweather
+ * The Player Model class handles the rendered playermodel
+ * @dateCreated May 24, 2018
+ * @dateCompleted June 5, 2018
+ * @version 1.0
+ */
 public class PlayerModel extends Entity {
 
-	public static final float HEIGHT = 1;
-	public static final float WIDTH = 1f;
+	public static final float HEIGHT = 0.75f;
+	public static final float WIDTH = 0.75f;
 
 	public Color teamColour;
 
+	/**
+	 * Constructor for a new playermodel with color, position, and the current modelbuilder
+	 * @param x - X position
+	 * @param y - Y position
+	 * @param z - Z position
+	 * @param teamColour - Team Colour
+	 * @param mb - Current modelbuilder
+	 */
 	public PlayerModel(float x, float y, float z, Color teamColour, ModelBuilder mb) {
 		super(x, y, z);
 		this.teamColour = teamColour;
 		model = new ModelInstance(mb.createBox(WIDTH, HEIGHT, WIDTH, new Material(ColorAttribute.createDiffuse(teamColour)), Usage.Position | Usage.Normal), x, y, z);
-	}
-
-	public void rotate(float pitch, float yaw) {
-		// model.transform.rotate(axis, degrees);
 	}
 
 }
