f8ffe84201615548eaa17cb2aeb0fd92fcb1ab3e
BlueEvil777 <37362426+BlueEvil777@users.noreply.github.com>
2018-06-07 13:49:41 -0600

    Add files via upload

--- 3DGame/desktop/src/com/dcprograming/game/core/desktop/DesktopLauncher.java
+++ 3DGame/desktop/src/com/dcprograming/game/core/desktop/DesktopLauncher.java
@@ -1,12 +1,36 @@
+/**
+ * Configures the libGDX lwjglApplication and starts it.
+ * 
+ * @author Colton Giesbrecht
+ * @dateCreated May 23, 2018
+ * @dateCompleted May 23, 2018
+ * @version 1.00
+ */
+
 package com.dcprograming.game.core.desktop;
 
+import java.awt.Toolkit;
+
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.dcprograming.game.core.Core;
 
+/**
+ * Configures and starts the lwjglApplication
+ */
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Core(), config);
+		int pixels = (int) Math.min(Toolkit.getDefaultToolkit().getScreenSize().getHeight() / 9, Toolkit.getDefaultToolkit().getScreenSize().getWidth() / 16);
+		config.width = pixels * 16;
+		config.height = pixels * 9;
+		System.out.println("Initial Screen Resolution: " + config.width + "x" + config.height);
+		config.resizable = false;
+		config.addIcon("Icon.png", FileType.Internal);
+		config.addIcon("Icon32.png", FileType.Internal);
+		config.title = "Quittage";
+		Core game = new Core(pixels);
+		new LwjglApplication(game, config);
 	}
 }
